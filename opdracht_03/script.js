3fb04de680261bf83af97b1d2ff02d7321c3c344
Jos Helmken <81860708+JosHelmken@users.noreply.github.com>
2021-05-29 22:24:06 +0200

    Add files via upload

---
+++ opdracht_03/script.js
@@ -0,0 +1,29 @@
+// Age check 
+
+const age = 36;
+
+if(age >= 18) {
+  console.log('Over 18 jaar dus je mag naar binnen!');
+} else {
+  console.log('Sorry geen 18, je mag niet naar binnen.');
+}
+
+// Ladiesnight
+
+const isFemale = false;
+
+if(isFemale) {
+  console.log('isFemale is True, dus kaartjes mogen verkocht worden');
+} else {
+  console.log('Sorry ladiesnight is alleen voor de dames');
+}
+
+// Bob
+
+const driverStatus = 'bob';
+
+if(driverStatus == 'bob') {
+  console.log('Jij bent vanavond de bob!');
+} else {
+  console.log('Sorry om thuis te komen heb je een bob nodig!');
+}
